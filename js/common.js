bf6cc2e4f855b2f4749784321287ec148984dfe8
sakakibara <sakakibara.miho@tecotec.co.jp>
2014-02-21 18:47:10 +0900

    開発事業途中

--- js/common.js
+++ js/common.js
@@ -102,3 +102,15 @@ $(window).on("load resize",function(){
   $('#btn-prev').css('top',arrowPositionY);
   $('#btn-next').css('top',arrowPositionY);
 });
+
+//するするスクロール
+$(function(){
+	$('a[href^=#]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
